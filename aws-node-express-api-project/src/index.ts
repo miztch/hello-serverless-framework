adde17a2680e78661cfd0ffd50b1c13126297826
miztch <miztch.me@gmail.com>
2023-04-13 01:36:43 +0900

    chapter06

--- aws-node-express-api-project/src/index.ts
+++ aws-node-express-api-project/src/index.ts
@@ -1,23 +1,24 @@
-const serverless = require("serverless-http");
-const express = require("express");
+import serverless from "serverless-http";
+import express from "express";
+
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/", (_req, res, _next) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
-app.get("/path", (req, res, next) => {
+app.get("/hello", (_req, res, _next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
 });
 
-app.use((req, res, next) => {
+app.use((_req, res, _next) => {
   return res.status(404).json({
     error: "Not Found",
   });
 });
 
-module.exports.handler = serverless(app);
+export const handler = serverless(app);
